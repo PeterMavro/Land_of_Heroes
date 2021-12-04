0b54fd51245f11f43dc98977d6a80356b677dd4e
Peter <Petermavromatakis@gmail.com>
2021-12-04 17:15:58 +1100

    Timer Added

---
+++ Gamejam_LandOfHeroes/Assets/Scripts/Timer.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class Timer : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    public int timerMinutes = 2;
+    public int timerseconds = 59;
+
+    private string displaySeconds;
+    private string displayMinutes;
+    public string displayFinished;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (timerseconds < 10)
+        {
+            displaySeconds = ":0" + timerseconds; 
+        }
+        else
+        {
+            displaySeconds = ":" + timerseconds; 
+        }
+        if (timerMinutes > 9)
+        {
+            displayMinutes = "" + timerMinutes;
+        }
+        else
+        {
+            displayMinutes = "0" + timerMinutes;
+        }
+        text.GetComponent<TextMeshProUGUI>().text = displayMinutes + displaySeconds;
+        StartCoroutine (Timerticking());
+
+        if (displayFinished == null)
+        {
+            displayFinished = "timer finished";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    IEnumerator Timerticking()
+    {
+        if (timerMinutes >= 0)
+        {
+            timerseconds -= 1;
+        }
+        if (timerseconds < 0)
+        {
+            timerMinutes -= 1;
+            timerseconds = 59;
+        }
+
+        yield return new WaitForSeconds(1);
+        if (timerseconds < 10)
+        {
+            displaySeconds = ":0" + timerseconds;
+        }
+        else
+        {
+            displaySeconds = ":" + timerseconds;
+        }
+        if (timerMinutes > 9)
+        {
+            displayMinutes = "" + timerMinutes;
+        }
+        else
+        {
+            displayMinutes = "0" + timerMinutes;
+        }
+        text.GetComponent<TextMeshProUGUI>().text = displayMinutes + displaySeconds;
+
+        if (timerMinutes < 0)
+        {
+            StopCoroutine(Timerticking());
+            text.GetComponent<TextMeshProUGUI>().text = displayFinished;
+        }
+        else
+        { 
+            StartCoroutine(Timerticking()); 
+        }
+    }
+}
